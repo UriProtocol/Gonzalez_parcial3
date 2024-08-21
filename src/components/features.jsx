62e33788ff250c40f7037dac48625cfe640af5ee
UrielGon03 <uriel@advantedigital.com>
2024-08-21 03:12:58 -0600

    v1

---
+++ src/components/features.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Features = (props) => {
+  return (
+    <div id="features" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Nuestros productos</h2>
+          <p>
+            Contamos con una variada colección de gorras y accesorios, además ofrecemos bordados personalizados.
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="..." className="team-img" />
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
